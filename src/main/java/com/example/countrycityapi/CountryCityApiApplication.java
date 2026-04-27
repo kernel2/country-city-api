53a12fe99b2aa9fd193a51984e6fa87466fc0e21
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2026-04-27 15:35:48 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/countrycityapi/CountryCityApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.countrycityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCityApiApplication.class, args);
+    }
+}

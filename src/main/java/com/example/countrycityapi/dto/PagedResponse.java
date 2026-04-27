7f6a2ec84bcc748d2155d2b595df220be962946e
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2026-04-27 16:11:50 +0200

    add generic pagination response model

---
+++ src/main/java/com/example/countrycityapi/dto/PagedResponse.java
@@ -0,0 +1,108 @@
+package com.example.countrycityapi.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class PagedResponse<T> {
+
+    private List<T> content = new ArrayList<>();
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PagedResponse() {
+    }
+
+    public PagedResponse(List<T> content, int page, int size, long totalElements, int totalPages, boolean last) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.last = last;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PagedResponse<?> that)) {
+            return false;
+        }
+        return page == that.page
+                && size == that.size
+                && totalElements == that.totalElements
+                && totalPages == that.totalPages
+                && last == that.last
+                && Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content, page, size, totalElements, totalPages, last);
+    }
+
+    @Override
+    public String toString() {
+        return "PagedResponse{" +
+                "content=" + content +
+                ", page=" + page +
+                ", size=" + size +
+                ", totalElements=" + totalElements +
+                ", totalPages=" + totalPages +
+                ", last=" + last +
+                '}';
+    }
+}
